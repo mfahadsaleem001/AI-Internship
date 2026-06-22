102e20a66202d6918c3fb0a0824833cb31a0b30d
Muhammad Fahad Saleem <fs4903166@gmail.com>
2026-06-22 13:20:29 +0500

    Day 1 Python practice

---
+++ Day_1/1_day.py
@@ -0,0 +1,27 @@
+name = "Fahad"
+
+subjects = ["Math", "English", "AI"]
+
+student = {
+    "name": "Fahad",
+    "roll_no": "F23-0421",
+    "program": "SE"
+}
+
+posts = [
+    {
+        "ID": 1,
+        "Title": "AI Agents",
+        "Author": "Mr Subhan"
+    },
+    {
+        "ID": 2,
+        "Title": "Monotholic",
+        "Author": "Mr Afaq"
+    }
+]
+
+print(name)
+print(subjects)
+print(student)
+print(posts)
