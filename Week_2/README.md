37dc67477d5162418d2179fef806833f9ae778e4
Muhammad Fahad Saleem <mfahadsaleem@gmail.com>
2026-07-03 11:46:15 +0500

    Complete Week 2-----> Data Collection piplines!

---
+++ Week_2/README.md
@@ -0,0 +1,8 @@
+<!-- Description -->
+
+This project demonstrates data collection using JSON APIs and web scraping with Python. The scraper collects quotes and authors from multiple pages, handles pagination, removes duplicate records, manages missing text, and exports the data to a CSV file.
+
+<!-- Output -->
+
+posts.csv
+Quotes.csv
--- Week_2/crawler.py
+++ Week_2/crawler.py
@@ -1,14 +1,56 @@
+# import requests
+# import pandas as pd
+
+# API URL
+# url = "https://jsonplaceholder.typicode.com/posts"
+
+# response = requests.get(url)
+# print(response.json())
+
+# posts = response.json()
+
+# df = pd.DataFrame(posts)
+# print(df)
+
+# df.to_csv("posts.csv", index = False)
+
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 
-url = "https://jsonplaceholder.typicode.com/posts"
+data = []
 
-response = requests.get(url)
-print(response.json())
+for page in range(1, 11):
+    # HTML Website
+    url = f"https://quotes.toscrape.com/page/{page}/"
+    response = requests.get(url)
 
-posts = response.json()
+    soup = BeautifulSoup(response.text, "html.parser")
 
-df = pd.DataFrame(posts)
-print(df)
+    quotes = soup.find_all("span", class_ = "text")
+    authors = soup.find_all("small", class_ = "author")
 
-df.to_csv("posts.csv", index = False)
+    for quote, author in zip(quotes, authors):
+        if quote is not None:
+            quote_text = quote.text
+        else:
+            quote_text = "Not Found"
+
+        if author is not None:
+            author_text = author.text
+        else:
+            author_text = "Not Found"
+
+        data.append(
+        {
+        "Quote": quote_text,
+        "Author": author_text
+        }
+)
+        print("Quote:", quote_text)
+        print("Author:", author_text)
+        print()
+    
+df = pd.DataFrame(data)
+df.drop_duplicates(inplace = True)
+df.to_csv("Quotes.csv", index = False)
