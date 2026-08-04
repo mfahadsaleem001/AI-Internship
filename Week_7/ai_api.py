d8cea255d15171726bd81fcad54aa0a8aab2efc1
Muhammad Fahad Saleem <mfahadsaleem@gmail.com>
2026-08-04 18:51:08 +0500

    Week 7 Day 2: Added POST API, Pydantic model and request body handling

--- Week_7/ai_api.py
+++ Week_7/ai_api.py
@@ -1,4 +1,6 @@
+# Day 1
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,3 +11,25 @@ def root():
     "status" : "Success",
     "data" : "API"
 }
+
+# Day 2
+@app.get("/user/{id}")
+def user(id:int):
+    return {
+        "user_id" : id,
+    }
+
+@app.get("/search")
+def search(name: str):
+    return {
+        "name" : name,
+    }
+
+class Post(BaseModel):
+    text: str
+    
+@app.post("/predict")
+def predict(post : Post):
+    return {
+        "text": post.text
+    }
